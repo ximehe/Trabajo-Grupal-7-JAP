8eacf387f2d6361cfd92a04bfd84faf160350f97
facundoborrazas <hernanborrazas@gmail.com>
2022-11-10 14:07:41 -0300

    Commit 3

--- CRUD/scripts/main.js
+++ CRUD/scripts/main.js
@@ -1,17 +1,35 @@
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    document.getElementById('btnGet1').addEventListener('click', ()=>{
+    document.getElementById('btnGet1').addEventListener('click', () => { //Buscador de registro
         let id = document.getElementById('inputGet1Id').value;
-        if( id !="" || id>0){
+        if (id != "" || id > 0) { //Si el id es distinto de vacio o mayor de 0 
+            fetch('https://6363eb3a8a3337d9a2ec6c3b.mockapi.io/users/' + id) //Llama al fetch con el id ingresado en el input
+                .then(response => response.json())
+                .then(data => document.getElementById('results').innerHTML =
+                    `
+                        <li>
+                            <p>ID: ${data.id}</p>
+                            <p>NAME: ${data.name}</p>
+                            <p>LASTNAME: ${data.lastname}</p>
+                        </li>
+                    `) //Se genera un elemento de la lista con el id, el nombre y el apellido del id ingresado
 
-            fetch('https://6363eb3a8a3337d9a2ec6c3b.mockapi.io/users/' + id)
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('results').innerHTML= 
-                data.name + "<br>" + 
-                data.lastname + "<br>" + data.id;
-        
-            });
+        } else if (id === "") { //Si el id es vacio
+            fetch('https://6363eb3a8a3337d9a2ec6c3b.mockapi.io/users/') //LLama al fetch
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById('results').innerHTML = ""; //Vacía todo lo que haya en la etiqueta con id="results"
+                    for (i = 0; i < data.length; i++) { //Se genera un for para iterar todos los registros que hay
+                        document.getElementById('results').innerHTML +=
+                            `
+                                <li>
+                                    <p>ID: ${data[i].id}</p>
+                                    <p>NAME: ${data[i].name}</p>
+                                    <p>LASTNAME: ${data[i].lastname}</p>
+                                </li>
+                            `//Se va generando tantos elementos de la lista como registros haya, cada uno con su respectivo ID, NAME y LASTNAME
+                    }
+                })
         }
     })
 
