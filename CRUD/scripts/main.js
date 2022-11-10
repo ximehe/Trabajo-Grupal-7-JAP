0044eeea03499854de711c53faa8517691aaf1a1
MaraCeciliaDev <mara.c.fer@gmail.com>
2022-11-10 14:00:04 -0300

    Commit 2

--- CRUD/scripts/main.js
+++ CRUD/scripts/main.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", ()=>{
     document.getElementById('btnGet1').addEventListener('click', ()=>{
         let id = document.getElementById('inputGet1Id').value;
@@ -5,7 +6,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
             fetch('https://6363eb3a8a3337d9a2ec6c3b.mockapi.io/users/' + id)
             .then(response => response.json())
-            .then(data => document.getElementById('results').innerHTML= data.name + "<br>" + data.lastname + "<br>" + data.id)
+            .then(data => {
+                document.getElementById('results').innerHTML= 
+                data.name + "<br>" + 
+                data.lastname + "<br>" + data.id;
+        
+            });
         }
     })
 
@@ -13,7 +19,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
         let nombre =document.getElementById('inputPostNombre').value;
         let appellido =document.getElementById('inputPostApellido').value;
-    
+
         fetch('https://6363eb3a8a3337d9a2ec6c3b.mockapi.io/users/',{
             headers: {"Content-Type":"application/json; charset=utf-8"},
             method: 'POST',
@@ -24,6 +30,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
         })
         .then(response => response.json())
         .then(data =>{
+            console.log(data)
+        })
+        .catch((error)=> {
+            console.log(error);
     
         })
     });
@@ -31,23 +41,67 @@ document.addEventListener("DOMContentLoaded", ()=>{
     document.getElementById('btnPut').addEventListener('click',()=>{
 
         let id =document.getElementById('inputPutId').value;
-    
-    
+        let nombre =document.getElementById('inputPostNombre').value;
+        let appellido =document.getElementById('inputPostApellido').value;
+        let data = {};
+        if (nombre) {
+            data.name = nombre;
+        }
+        if (appellido) {
+            data.lastname = appellido
+        };
+
+
         fetch('https://6363eb3a8a3337d9a2ec6c3b.mockapi.io/users/',{
             headers: {"Content-Type":"application/json; charset=utf-8"},
             method: 'PUT',
-            body: JSON.stringify({
-                name: nombre,
-                lastname: appellido
-            }) 
+            body: JSON.stringify(data) 
         })
         .then(response => response.json())
         .then(data =>{
-    
+            console(data);
         })
+        .catch((error) => {
+            console.log(error)
+        })
+
+       
     });
 
+    document.getElementById('btnDelete').addEventListener('click',()=> {
+
+        let id =document.getElementById('inputDelete').value;
+    
+        fetch('https://6363eb3a8a3337d9a2ec6c3b.mockapi.io/users/' + id,{
+            headers: {"Content-Type":"application/json; charset=utf-8"},
+            method: 'DELETE',
+            })
+            .then(response => response.json())
+            .then(data =>{
+                console(data);
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    
+    });  
+
+    document.getElementById('btnDelete').addEventListener('click',()=> {
+
+        let id =document.getElementById('inputDelete').value;
+    
+        fetch('https://6363eb3a8a3337d9a2ec6c3b.mockapi.io/users/' + id,{
+            headers: {"Content-Type":"application/json; charset=utf-8"},
+            method: 'PUT',
+            })
+            .then(response => response.json())
+            .then(data =>{
+                console(data);
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    
+    });  
+
 })
-
-
-
